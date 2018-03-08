f99c5530157b619b3d76dcb8107b75fb06bd7eb9
mhirschmann <mhirschmann@national.aaa.com>
2018-03-08 17:15:35 -0500

    dropped code

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -43,72 +43,7 @@ $(document).ready(function() {
     
     });  //  Closes jQuery wrapper
     
-    function generateLossDueToTimeOut() {
-        unansweredTally++;
-        gameHTML = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>" + counter + "</span></p>" + "<p class='text-center'>You ran out of time!  The correct answer was: " + correctAnswers[questionCounter] + "</p>" + "<img class='center-block img-wrong' src='img/x.png'>";
-        $(".mainArea").html(gameHTML);
-        setTimeout(wait, 4000);  //  change to 4000 or other amount
-    }
     
-    function generateWin() {
-        correctTally++;
-        gameHTML = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>" + counter + "</span></p>" + "<p class='text-center'>Correct! The answer is: " + correctAnswers[questionCounter] + "</p>" + imageArray[questionCounter];
-        $(".mainArea").html(gameHTML);
-        setTimeout(wait, 4000);  //  change to 4000 or other amount
-    }
-    
-    function generateLoss() {
-        incorrectTally++;
-        gameHTML = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>" + counter + "</span></p>" + "<p class='text-center'>Wrong! The correct answer is: "+ correctAnswers[questionCounter] + "</p>" + "<img class='center-block img-wrong' src='img/x.png'>";
-        $(".mainArea").html(gameHTML);
-        setTimeout(wait, 4000); //  change to 4000 or other amount
-    }
-    
-    function generateHTML() {
-        gameHTML = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>30</span></p><p class='text-center'>" + questionArray[questionCounter] + "</p><p class='first-answer answer'>A. " + answerArray[questionCounter][0] + "</p><p class='answer'>B. "+answerArray[questionCounter][1]+"</p><p class='answer'>C. "+answerArray[questionCounter][2]+"</p><p class='answer'>D. "+answerArray[questionCounter][3]+"</p>";
-        $(".mainArea").html(gameHTML);
-    }
-    
-    function wait() {
-        if (questionCounter < 7) {
-        questionCounter++;
-        generateHTML();
-        counter = 30;
-        timerWrapper();
-        }
-        else {
-            finalScreen();
-        }
-    }
-    
-    function timerWrapper() {
-        theClock = setInterval(thirtySeconds, 1000);
-        function thirtySeconds() {
-            if (counter === 0) {
-                clearInterval(theClock);
-                generateLossDueToTimeOut();
-            }
-            if (counter > 0) {
-                counter--;
-            }
-            $(".timer").html(counter);
-        }
-    }
-    
-    function finalScreen() {
-        gameHTML = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>" + counter + "</span></p>" + "<p class='text-center'>All done, here's how you did!" + "</p>" + "<p class='summary-correct'>Correct Answers: " + correctTally + "</p>" + "<p>Wrong Answers: " + incorrectTally + "</p>" + "<p>Unanswered: " + unansweredTally + "</p>" + "<p class='text-center reset-button-container'><a class='btn btn-primary btn-lg btn-block reset-button' href='#' role='button'>Reset The Quiz!</a></p>";
-        $(".mainArea").html(gameHTML);
-    }
-    
-    function resetGame() {
-        questionCounter = 0;
-        correctTally = 0;
-        incorrectTally = 0;
-        unansweredTally = 0;
-        counter = 30;
-        generateHTML();
-        timerWrapper();
-    }
     
     var startScreen;
     var gameHTML;
